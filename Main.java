52a39a6b2dde2d308ef98cc6a25f350c01a6653b
Soham Raj Jain <68377091+Soham-Raj-Jain@users.noreply.github.com>
2024-07-03 13:58:29 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// creating object of main class
+		Calcullator main = new Calcullator();
+		
+		
+		// calling the calculator function from the class
+		main.calculator();
+
+	}
+
+} 
+    
+
